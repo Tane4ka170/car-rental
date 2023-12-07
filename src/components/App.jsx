c61a57e19d84b8d0a2b5b2c9d01c1ce6b606ea13
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2023-12-07 13:32:11 +0200

    layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,12 @@ import { Route, Routes } from "react-router";
 import Home from "../pages/Home/Home";
 import Catalog from "../pages/Catalog/Catalog";
 import Favorites from "../pages/Favorites/Favorites";
+import Layout from "./Layout/Layout";
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/favorites" element={<Favorites />} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,58 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-        aliquid amet fuga voluptatem, voluptatibus, laborum ipsam debitis fugit
-        explicabo facere sunt beatae rem voluptatum eos recusandae molestias
-        doloribus consequatur in!
-      </p>
+      <h1>Rent a New Car in Ukraine with Address Delivery in Just One Hour</h1>
+      <section>
+        <div>
+          <h2>We offer the following services for your convenience</h2>
+          <ul>
+            <li>
+              Car Insurance
+              <p>
+                Travel with peace of mind as we manage all risks with valid
+                insurance policies for all our cars.
+              </p>
+            </li>
+            <li>
+              GPS Navigation
+              <p>
+                Never worry about getting lost; our GPS navigation devices will
+                calculate the best route for you, saving you time.
+              </p>
+            </li>
+            <li>
+              Child Safety Seat
+              <p>
+                Ensure the safety of your little ones during travel with our
+                comfortable child safety seats.
+              </p>
+            </li>
+            <li>
+              Wi-Fi
+              <p>
+                Stay connected on the go with high-speed Wi-Fi internet in our
+                vehicles.
+              </p>
+            </li>
+            <li>
+              Concierge Service
+              <p>
+                Enjoy round-the-clock support for any inquiries, whether it's
+                hotel reservations or assistance in finding the best tours and
+                products.
+              </p>
+            </li>
+            <li>
+              Roadside Assistance{" "}
+              <p>
+                Whether you've run out of fuel, need a broken wheel replaced, or
+                your battery needs charging, contact us, and our team will
+                promptly arrive to assist you on the road.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 };
